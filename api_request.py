2737c044e0ce1c340fa7673704e966d192fd8fcd
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2019-06-04 19:13:23 -0500

    Consulta de carta

--- api_request.py
+++ api_request.py
@@ -1,4 +1,6 @@
 import requests
+import json
+
 scryfall = "https://api.scryfall.com"
 github = "https://api.github.com"
 carta_nombre = "/cards/named?fuzzy="
@@ -11,3 +13,9 @@ war._content
 
 eternal_witness.status_code
 eternal_witness._content
+
+ew_json = json.loads(eternal_witness._content)
+ew_json["flavor_text"]
+
+ew_json["cmc"] <= 2
+"Creature" in ew_json["type_line"]
