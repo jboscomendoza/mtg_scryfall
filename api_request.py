3aac196fdbc3402ac2b3a66c5aff82d4a083b837
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2019-06-04 23:52:58 -0500

    Funciónn para leer decklists con formato mtgo, 2 decklist de prueba

--- api_request.py
+++ api_request.py
@@ -1,8 +1,31 @@
 import requests
 import json
+import re
 
+# Lectura de decklist
+# "S" al inicio indica que inicia texto de "Sideboard"
+def leer_decklist(archivo):
+    deck = open(archivo, "r")
+    deck = deck.readlines()
+    i = 0
+    decklist = []
+    while not deck[i].startswith("S"):
+        linea = deck[i]
+        nombre = re.sub("^(\d)+ ", "", linea)
+        nombre = re.sub("\n", "", nombre)
+        cantidad = re.sub(" .*", "", linea)
+        cantidad = int(cantidad)
+        i += 1
+        decklist.append([cantidad, nombre])
+    return(decklist)
+
+ruta_infect = "Modern_Infect_by_sirpuffsalot.txt"
+ruta_rdw = "Standard_Red_Deck_Wins_by_Adam_Bink.txt"
+leer_decklist(ruta_infect)
+leer_decklist(ruta_rdw)
+
+# Consulta al API de scryfall
 scryfall = "https://api.scryfall.com"
-github = "https://api.github.com"
 carta_nombre = "/cards/named?fuzzy="
 
 war = requests.get(scryfall + "/sets/war")
@@ -19,3 +42,5 @@ ew_json["flavor_text"]
 
 ew_json["cmc"] <= 2
 "Creature" in ew_json["type_line"]
+
+
