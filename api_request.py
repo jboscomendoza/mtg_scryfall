d48ee8764ecb2a283c2dee087a422230b129ef62
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2019-06-05 00:56:55 -0500

    json para obtener collection de cartas

--- api_request.py
+++ api_request.py
@@ -21,12 +21,13 @@ def leer_decklist(archivo):
 
 ruta_infect = "Modern_Infect_by_sirpuffsalot.txt"
 ruta_rdw = "Standard_Red_Deck_Wins_by_Adam_Bink.txt"
-leer_decklist(ruta_infect)
-leer_decklist(ruta_rdw)
+deck_infect = leer_decklist(ruta_infect)
+deck_rdw = leer_decklist(ruta_rdw)
 
 # Consulta al API de scryfall
 scryfall = "https://api.scryfall.com"
 carta_nombre = "/cards/named?fuzzy="
+collection = "/cards/collection"
 
 war = requests.get(scryfall + "/sets/war")
 eternal_witness = requests.get(scryfall + carta_nombre + "Eternal Witness")
@@ -43,4 +44,21 @@ ew_json["flavor_text"]
 ew_json["cmc"] <= 2
 "Creature" in ew_json["type_line"]
 
+cartas_lista = {"identifiers":[
+    {"name": "Shock"},
+    {"name": "Viashino Pyromancer"}
+]}
 
+requests.post("https://api.scryfall.com/cards/collection", json=cartas_lista)
+
+rdw_json = []
+for carta in deck_rdw:
+    llave = "{\"name\": \"" + carta[1] + "\"}"
+    rdw_json.append(llave)
+rdw_json = ", ".join(rdw_json)
+rdw_json = "{\"identifiers\":[" + rdw_json + "]}"
+rdw_json = json.loads(rdw_json)
+
+rdw_collection = requests.post("https://api.scryfall.com/cards/collection", json=rdw_json)
+
+rdw_collection._content
