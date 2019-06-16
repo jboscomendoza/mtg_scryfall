8082820d57d9d82afa1ec70247c1e9c627ae52e1
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2019-06-16 16:15:20 -0500

    Layout y carga de decklist

--- gui.py
+++ gui.py
@@ -3,110 +3,103 @@ import api_request as mtg
 from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication, QMainWindow, QApplication, QAction, qApp, QTextEdit, QLabel, QFileDialog, QGridLayout, QLineEdit)
 from PyQt5.QtGui import QFont, QIcon
 
-
-class VentanaPrincipal(QMainWindow):
+class App(QWidget):
     
     def __init__(self):
         super().__init__()
         
-        self.setWindowTitle("MTG App")
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowIcon(QIcon('mtg_icon.png'))
+        self.lbl_deck = QLabel(u'''
+        Carga un decklist en formato .txt
+        ''')
+        self.lbl_sim = QLabel(u'''
+        Simulación no iniciada
+        ''')
+        
+        self.btn_cargar = QPushButton("Cargar deklist", self)
+        self.btn_cargar.clicked.connect(self.showDialog)
+        self.btn_cargar.setToolTip("Carga un decklist")
+        
+        self.btn_simular = QPushButton(u"Iniciar simulación")
+
+        self.btn_cerrar = QPushButton("&Salir")
+        self.btn_cerrar.setShortcut('Ctrl+Q')
+        self.btn_cerrar.setStatusTip('Salir de la App')
+        self.btn_cerrar.clicked.connect(qApp.quit)
+
+        self.var_deck = []
 
         self.initUI()
-        
+
     def initUI(self):
-        
-        QToolTip.setFont(QFont('SansSerif', 10))
-                
         newAct = QAction(QIcon("icons/file-plus.svg"), "Nuevo", self)
         newAct.setShortcut("Ctrl+N")
         newAct.setStatusTip("Nueva decklist")
         newAct.triggered.connect(self.showDialog)
         
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(self.btn_cargar, 1, 0)
+        grid.addWidget(self.btn_simular, 1, 1)
+        grid.addWidget(self.lbl_deck, 2, 0)
+        grid.addWidget(self.lbl_sim, 2, 1)
+        grid.addWidget(self.btn_cerrar, 3, 0, 1, 2)
+
+        self.setLayout(grid)
+        
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('MTG App')
+        self.setWindowIcon(QIcon('mtg_icon.png'))
+
+        self.show()
+    
+    def showDialog(self):
+        archivo = QFileDialog.getOpenFileName(self, "Nuevo decklist", "/home")
+        if archivo[0]:
+            deck_plain = mtg.leer_plain_deck(archivo[0])
+            deck_text = mtg.get_decklist_text(deck_plain)
+            deck_text = "".join(deck_text)
+
+            self.lbl_deck.setText(deck_text)
+            self.var_deck = deck_plain
+
+
+class AppMenu(QWidget):
+    
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+     
+    def initMenu(self):
+        newAct = QAction(QIcon("icons/file-plus.svg"), "Nuevo", self)
+        newAct.setShortcut("Ctrl+N")
+        newAct.setStatusTip("Nueva decklist")
+        newAct.triggered.connect(self.showDialog)
         simAct = QAction(QIcon("icons/shuffle.svg"), "Simular", self)
         simAct.setShortcut("Ctrl+I")
         simAct.setStatusTip("Iniciar simulacion")
-        
+
         guardarAct = QAction(QIcon("icons/save.svg"), "Guardar", self)
         guardarAct.setShortcut("Ctrl+G")
         guardarAct.setStatusTip("Guardar simulacion")
-
         exitAct = QAction(QIcon('icons/x-square.svg'), '&Salir', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Salir de la App')
         exitAct.triggered.connect(qApp.quit)
 
-        self.statusBar().showMessage("Ok")
-        
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu("&Archivo")
-        fileMenu.addAction(newAct)
-        fileMenu.addAction(guardarAct)
-        fileMenu.addAction(exitAct)
-        simMenu = menubar.addMenu("&Simulacion")
-        simMenu.addAction(simAct)
-        
-        self.toolbar = self.addToolBar("Archivo")
-        self.toolbar.addAction(newAct)
-        self.toolbar.addAction(guardarAct)
-        self.toolbar.addAction(exitAct)
-        self.toolbar = self.addToolBar("Simulacion")
-        self.toolbar.addAction(simAct)
-
-        self.red = RedLayout(self)
-        self.setCentralWidget(self.red.initGrid())
-
-
-        # boton = QPushButton('Boton para presionar', self)
-        # boton.setToolTip('Este es un boton chido')
-        # boton.resize(boton.sizeHint())
-        # boton.move(50, 50)
-
-        #boton_salida = QPushButton("Salir", self)
-        #boton_salida.clicked.connect(QApplication.instance().quit)
-        #boton_salida.setToolTip("Este boton cierra")
-        #boton.resize(boton_salida.sizeHint())
-        #boton.move(50, 50)
-        
-        self.show()
-    
-    def showDialog(self):
-        fname = QFileDialog.getOpenFileName(self, "Nuevo decklist", "/home")
-        if fname[0]:
-            f = mtg.leer_plain_deck(fname[0])
-            self.red.initGrid.lbl_trials.setText(f[0])
-
-class RedLayout(QWidget):
-    
-    def __init__(self, parent):
-        super(QWidget, self).__init__(parent)
-     
-    def initGrid(self):
-        
-        lbl_decklist = QLabel('Decklist vacio', self)
-        lbl_trials = QLabel("Numero de trials", self)
-        edit_decklist = QLineEdit()
-        edit_trials = QLineEdit()
-
-        grid = QGridLayout()
-        grid.setSpacing(10)
-
-        grid.addWidget(lbl_decklist, 1, 0)
-        grid.addWidget(edit_decklist, 1, 1)
-        grid.addWidget(lbl_trials, 2, 0)
-        grid.addWidget(edit_trials, 2, 1)
-
-        self.setLayout(grid)
+        toolbar = self.addToolBar("Archivo")
+        toolbar.addAction(newAct)
+        toolbar.addAction(guardarAct)
+        toolbar.addAction(exitAct)
+        toolbar = self.addToolBar("Simulacion")
+        toolbar.addAction(simAct)
 
         self.setGeometry(10, 50, 200, 100)
-        self.setLayout(grid)
+        self.setLayout(toolbar)
 
         self.show()
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = VentanaPrincipal()
+    ex = App()
     sys.exit(app.exec_())
